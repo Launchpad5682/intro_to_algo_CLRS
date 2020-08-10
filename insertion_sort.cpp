0788cdf031367a338d3f692dcddca696ae4f8f13
Launchpad5682 <saurabh.189302124@muj.manipal.edu>
2020-08-10 09:34:43 +0530

    insertion sort

--- insertion_sort.cpp
+++ insertion_sort.cpp
@@ -1,7 +1,32 @@
-#include <iostream>
+#include <bits/stdc++.h>
 
 using namespace std;
 
 int main()
 {
+    int n, t;
+    cin >> n;
+    vector<int> arr(n);
+
+    for (int i = 0; i < n; i++)
+        cin >> arr.at(i);
+
+    for (int i = 1; i < n; i++)
+    {
+        int key = arr.at(i);
+        int j = i - 1;
+        while (j >= 0 && arr[j] > key)
+        {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = key;
+    }
+
+    for (auto i : arr)
+        cout << i << " ";
+
+    cout << endl;
+
+    return 0;
 }
